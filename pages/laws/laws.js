7af4216513d73be1b79265cb35dd1b97bb262f0f
chenweihong <15094846015@163.com>
2017-03-22 22:16:55 +0800

    专家报告和法律法规列表的调整
    修改专家详情样式，添加报告和法律列表组件

--- pages/laws/laws.js
+++ pages/laws/laws.js
@@ -1,8 +1,33 @@
 // pages/laws/laws.js
 Page({
-  data:{},
+  data:{
+    laws: [
+      {
+        title: '标题名称',
+        content: '法律法规的内容，比较简单的，只是为了显示效果，随便加点东西'
+      },
+      {
+        title: '标题名称',
+        content: '法律法规的内容，比较简单的，只是为了显示效果，随便加点东西'
+      },
+      {
+        title: '标题名称',
+        content: '法律法规的内容，比较简单的，只是为了显示效果，随便加点东西'
+      },
+      {
+        title: '标题名称',
+        content: '法律法规的内容，比较简单的，只是为了显示效果，随便加点东西'
+      },
+      {
+        title: '标题名称',
+        content: '法律法规的内容，比较简单的，只是为了显示效果，随便加点东西'
+      },
+    ]
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    // 如果是网络请求或是别的方法传出的值调用“this”,不能直接用，需要用变量接收“this” 再使用(如mine.js中的用法)
+    console.log(this.data.laws)
   },
   onReady:function(){
     // 页面渲染完成
